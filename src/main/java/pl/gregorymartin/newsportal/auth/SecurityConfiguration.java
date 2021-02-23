a43ec26db0b58dd65b3f4c0fcac40891612aebdf
Grzegorz Marcin Szczerba <vetomir@gmail.com>
2021-02-23 02:08:13 +0100

    cors config

--- src/main/java/pl/gregorymartin/newsportal/auth/SecurityConfiguration.java
+++ src/main/java/pl/gregorymartin/newsportal/auth/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package pl.gregorymartin.newsportal.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,35 +11,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import pl.gregorymartin.newsportal.appUser.AppUserService;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     private final TokenService tokenService;
     private UserDetailsService userDetailsService;
 
     SecurityConfiguration(TokenService tokenService, final UserDetailsService userDetailsService) {
         this.tokenService = tokenService;
         this.userDetailsService = userDetailsService;
+
+
     }
 
     @Autowired
@@ -70,9 +62,9 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/api/users/me").permitAll()
                 .antMatchers(HttpMethod.POST,"/api").permitAll()
                 .anyRequest()
-                .authenticated()
+                /*.authenticated()*/
                 /*heroku problems*/
-                /*.permitAll()*/
+                .permitAll()
                 .and()
                 .addFilterBefore(new AuthenticationFilter(userDetailsService, tokenService), AnonymousAuthenticationFilter.class)
                 .sessionManagement()
@@ -121,7 +113,8 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins(
-                                "https://portfolio-blog-react.vercel.app")
+                                "http://localhost:3001",
+                                "https://my-blog-app-view.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH");
             }
         };
